5ba273fabc3d4abcaea639211a2a441b22c40c15
nharris710 <noaharris248@gmail.com>
2023-02-13 23:56:57 -0800

    Calculate sales price

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ def main():
 
 if __name__ == '__main__':
     main()
+    print ("Regular Price 200") 
+original_price = int(200)
+rate = float(0.20)
+discount_amount = (rate * original_price)
+print('discount_amount: ',rate * original_price )
+rate = float("0.20")
+print('The final price:',original_price - discount_amount) 
