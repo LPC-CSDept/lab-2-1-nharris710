084f3347a9af236458e822e14b9834f239acd8ba
nharris710 <noaharris248@gmail.com>
2023-02-19 20:31:19 -0800

    cal sales price

--- main.py
+++ main.py
@@ -2,12 +2,7 @@ def main():
    ##################################################
    # Comlete your code here
    ##################################################
-
-    pass
-
-
-if __name__ == '__main__':
-    main()
+ 
     print ("Regular Price 200") 
 original_price = int(200)
 rate = float(0.20)
@@ -15,3 +10,9 @@ discount_amount = (rate * original_price)
 print('discount_amount: ',rate * original_price )
 rate = float("0.20")
 print('The final price:',original_price - discount_amount) 
+
+
+
+if __name__ == '__main__':
+    main()
+    
